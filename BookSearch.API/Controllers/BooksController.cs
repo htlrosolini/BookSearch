0fef2694c03aa1b7946a0667f23144e57997ce57
Henry Turner Lizidatti Rosolini <htlrosolini@hotmail.com>
2024-02-11 02:02:02 -0300

    Add project files.

---
+++ BookSearch.API/Controllers/BooksController.cs
@@ -0,0 +1,56 @@
+﻿using BookSearch.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookSearch.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        public BooksController(BooksContext context)
+        {
+            Context = context;
+        }
+
+        public BooksContext Context { get; }
+
+        [HttpGet]
+        public async Task<IEnumerable<Book>> Get([FromQuery] string property, [FromQuery] string value)
+        {
+            var books = await Context.Books.ToListAsync();
+
+            switch (property.ToLower())
+            {
+                case "title":
+                    return books.Where(b => b.Title == value);
+                case "firstname":
+                    return books.Where(b => b.FirstName == value );
+                case "lastname":
+                    return books.Where(b=> b.LastName == value );
+                case "totalcopies":
+                    return books.Where(b=> b.TotalCopies == Convert.ToInt32(value));
+                case "copiesinuse":
+                    return books.Where(b=>b.CopiesInUse == Convert.ToInt32(value));
+                case "type":
+                    return books.Where(b=> b.Type == value );
+                case "isbn":
+                    return books.Where(b => b.Isbn == value );
+                case "category":
+                    return books.Where(b => b.Category == value );
+                default:
+                    return books;
+            }
+        }             
+    }
+}                    
+
+                      
+
+                      
+
+                     
+
+                     
---
+++ BookSearch.API/Models/BooksContext.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookSearch.API.Models;
+
+public partial class BooksContext : DbContext
+{
+    public BooksContext()
+    {
+    }
+
+    public BooksContext(DbContextOptions<BooksContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Book> Books { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("name=constring");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasKey(e => e.BookId).HasName("PK__books__490D1AE156BA36CE");
+
+            entity.ToTable("books");
+
+            entity.Property(e => e.BookId).HasColumnName("book_id");
+            entity.Property(e => e.Category)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("category");
+            entity.Property(e => e.CopiesInUse).HasColumnName("copies_in_use");
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("first_name");
+            entity.Property(e => e.Isbn)
+                .HasMaxLength(80)
+                .IsUnicode(false)
+                .HasColumnName("isbn");
+            entity.Property(e => e.LastName)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("last_name");
+            entity.Property(e => e.Title)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("title");
+            entity.Property(e => e.TotalCopies).HasColumnName("total_copies");
+            entity.Property(e => e.Type)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("type");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
